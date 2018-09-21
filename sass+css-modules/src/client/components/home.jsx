ca2a85ef384c90f0294ae3954c7b9186cca59723
Sheng Di <sdi@walmartlabs.com>
2018-09-20 22:11:51 -0700

    update sass sample

---
+++ sass+css-modules/src/client/components/home.jsx
@@ -0,0 +1,54 @@
+/*
+ * This is a demo component the Eletrode app generator included
+ * to show using Milligram CSS lib and Redux
+ * store for display HTML elements and managing states.
+ *
+ * To start your own app, please replace or remove these files:
+ *
+ * - this file (home.jsx)
+ * - demo-buttons.jsx
+ * - demo-pure-states.jsx
+ * - demo-states.jsx
+ * - reducers/index.jsx
+ * - styles/*.css
+ *
+ */
+
+import React from "react";
+import { connect } from "react-redux";
+import custom from "../styles/custom.scss";
+
+//
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className={custom["navigation--button"]}>
+          <div className={custom["material--burger"]}>
+            <span />
+          </div>
+        </nav>
+
+        <h1>
+          SCSS <span>Material Design Menu-Button</span>
+        </h1>
+      </div>
+    );
+  }
+}
+
+Home.propTypes = {};
+
+const mapStateToProps = () => {
+  return {};
+};
+
+export default connect(
+  mapStateToProps,
+  dispatch => ({ dispatch })
+)(Home);
