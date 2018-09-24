2ea9b494d7d691b655ac01ffa72561c6504a51c8
Sheng Di <sdi@walmartlabs.com>
2018-09-24 10:45:59 -0700

    add css example

---
+++ css/README.md
@@ -0,0 +1,32 @@
+# css-sample [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+
+>
+
+## Installation
+
+```sh
+$ npm install --save css-sample
+```
+
+## Usage
+
+```js
+var cssSample = require("css-sample");
+
+cssSample("Rainbow");
+```
+
+## Preview
+
+![Alt text](../preview/css.png?raw=true "Title")
+
+## License
+
+Apache-2.0 © [Sheng Di]()
+
+[npm-image]: https://badge.fury.io/js/css-sample.svg
+[npm-url]: https://npmjs.org/package/css-sample
+[travis-image]: https://travis-ci.org/didi0613/css-sample.svg?branch=master
+[travis-url]: https://travis-ci.org/didi0613/css-sample
+[daviddm-image]: https://david-dm.org/didi0613/css-sample.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/didi0613/css-sample
---
+++ css/src/client/components/home.jsx
@@ -0,0 +1,91 @@
+/*
+ * This is a demo component the Eletrode app generator included
+ * to show using Milligram CSS lib and Redux
+ * store for display HTML elements and managing states.
+ *
+ * To start your own app, please replace or remove these files:
+ *
+ * - this file (home.jsx)
+ * - demo-buttons.jsx
+ * - demo-pure-states.jsx
+ * - demo-states.jsx
+ * - reducers/index.jsx
+ * - styles/*.css
+ *
+ */
+
+import React from "react";
+import { connect } from "react-redux";
+import "../styles/custom.css";
+
+//
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>
+          CSS3 Transition examples (on <kbd>:hover</kbd>)
+        </h1>
+        <div className="wrap">
+          <div className="box">
+            <h2>Text-shadow</h2>
+          </div>
+          <div className="box">
+            <div className="one">
+              <kbd>border-color, border-radius, background-color</kbd>
+            </div>
+          </div>
+          <div className="box">
+            <div className="two">
+              <kbd>border-style, background-color</kbd>
+            </div>
+          </div>
+          <div className="box">
+            <div className="three">
+              <kbd>outline-color, outline-offset</kbd>
+            </div>
+          </div>
+          <div className="box">
+            <div className="four">
+              <kbd>width, height, box-shadow</kbd>
+            </div>
+          </div>
+          <div className="box">
+            <div className="five">
+              <kbd>position</kbd>
+            </div>
+          </div>
+          <div className="box">
+            <img src="https://html5book.ru/wp-content/uploads/2016/01/css3-filters.jpg" />
+          </div>
+          <div className="box">
+            <div className="six">
+              <kbd>background-position</kbd>
+            </div>
+          </div>
+          <div className="box">
+            <div className="seven">
+              <kbd>background-size</kbd>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Home.propTypes = {};
+
+const mapStateToProps = () => {
+  return {};
+};
+
+export default connect(
+  mapStateToProps,
+  dispatch => ({ dispatch })
+)(Home);
